68353995fb4924e5fe3a70707c59adcf902ad727
monge <pedrokamonge@gmail.com>
2026-04-06 17:33:05 -0300

    primeiro commit

---
+++ ellev/Home.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ellev
+//
+//  Created by PEDRO MONGE SILVEIRA on 02/04/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ ellev/SignUp.swift
@@ -0,0 +1,18 @@
+//
+//  firstLogin.swift
+//  ellev
+//
+//  Created by PEDRO MONGE SILVEIRA on 06/04/26.
+//
+
+import SwiftUI
+
+struct signUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    firstLogin()
+}
