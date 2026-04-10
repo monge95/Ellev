c3cbb437f73c79976f89b56e2007a31abbc4ed0f
pedro monge <pedrokamonge@gmail.com>
2026-04-10 17:13:57 -0300

    andamento do agendamento funcionando

---
+++ ellev/AgendarPilotos.swift
@@ -0,0 +1,23 @@
+//
+//  AgendarPilotos.swift
+//  ellev
+//
+//  Created by PEDRO MONGE SILVEIRA on 10/04/26.
+//
+
+import SwiftUI
+
+struct AgendarPilotos: View {
+    var body: some View {
+        NavigationStack {
+                
+            Text("Selecione o piloto")
+                .font(Font.custom("jost", size: 22))
+        }
+        
+    }
+}
+
+#Preview {
+    AgendarPilotos()
+}
---
+++ ellev/componentes/AndamentoCompra.swift
@@ -0,0 +1,106 @@
+//
+//  AndamentoCompra.swift
+//  ellev
+//
+//  Created by PEDRO MONGE SILVEIRA on 10/04/26.
+//
+
+import SwiftUI
+
+struct Andamento: View {
+    let numero: Int
+    let tamanho: CGFloat = 30
+    let corFundo: Color
+    let corTexto: Color
+    
+    var body: some View {
+        Circle()
+            .fill(corFundo)
+            .frame(width: tamanho, height: tamanho)
+        
+            .overlay(
+                Text("\(numero)")
+                    .font(.system(size: 16))  // Fonte proporcional
+                    .foregroundColor(corTexto)
+                    .bold()
+            )
+    }
+}
+
+struct AndamentoCompra: View {
+var QuantidadeTelas: Int
+var tela:  Int
+    var body: some View {
+        ZStack {
+            VStack{
+                HStack(spacing: 0){
+                    
+                    ForEach(1...QuantidadeTelas, id: \.self){index in
+                        
+                        if tela >= index {
+                            Andamento(numero: index, corFundo: .button, corTexto: .fundodeTelas)
+                            
+                            if tela == index && QuantidadeTelas != index {
+                                ZStack(alignment: .leading) {
+                                    
+                                    if index != QuantidadeTelas {
+                                        
+                                        Rectangle()
+                                            .frame(height: 1)
+                                            .foregroundColor(.fundodeTelas)
+                                    }
+                                    
+                                    Image("beijaflorAzul")
+                                    Rectangle()
+                                        .frame(width: 10,height: 1)
+                                        .foregroundColor(.button)
+                                }
+                                
+                            }
+                            if index != tela {
+                                
+                                Rectangle()
+                                    .frame(height: 1)
+                                    .foregroundColor(.button)
+                            }
+                            
+                            
+                        }else{
+                            Andamento(numero: index, corFundo: .inputButton, corTexto: .button)
+                            if index != QuantidadeTelas {
+                                
+                                Rectangle()
+                                    .frame(height: 1)
+                                    .foregroundColor(.inputButton)
+                            }else{
+                              Spacer(minLength: 30)
+                            }
+                            
+                        }
+                        
+                        
+                        
+                        
+                    }
+                    
+                }
+                .padding(.horizontal, 54)
+                
+         
+                HStack{
+                    Text("ola ")
+                    Spacer(minLength: 86)
+                    Text("mundo")
+                    Spacer()
+                    Text("bem vindo")
+                }
+                .padding(.horizontal, 54)
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    AndamentoCompra(QuantidadeTelas: 3, tela: 3)
+}
