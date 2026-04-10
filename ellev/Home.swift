4ccdca252d590d2f09bee4e10c52fdb032f239ec
João Cláudio <leiloka46@gmail.com>
2026-04-10 18:05:37 -0300

    feat: Tela Home

--- ellev/Home.swift
+++ ellev/Home.swift
@@ -6,55 +6,116 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct Home: View {
     @State public var Acompanhates: Bool = false
+    @State private var departureInput: String = ""
+    @State private var arrivalInput: String = ""
+    
+    let cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
+    let locationManager = CLLocationManager()
     
     var body: some View {
         NavigationStack {
             ZStack{
                 Color("FundodeTelasColor")
                     .edgesIgnoringSafeArea(.all)
+                
                 ScrollView{
                     
-                 
-                     Spacer(minLength: 30)
-                        Image("logoPrincipal")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 227, height: 133)
+                    VStack(alignment: .leading, spacing: 20) {
                         
-                     Spacer()
+                        HStack {
+                            Spacer()
+                            Image("logoPrincipal")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 227, height: 133)
+                            Spacer()
+                        }
+                        
                         Text("Viaje com a gente e aproveite a melhor forma de se locomover por São Paulo")
-                        .font(Font.custom("Helvetica", size: 16))
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.tex)
-                        .lineLimit(nil)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .frame(maxWidth: 243, maxHeight: 54, alignment: .center)
+                            .font(Font.custom("Helvetica-Oblique", size: 16))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.tex)
+                            .padding(.bottom, 20)
+                            .frame(maxWidth: .infinity, alignment: .center)
                         
-                     Spacer()
-                        Text("Escolha seu local e data de viajem ")
-                            .font(Font.custom("Helvetica-body", size: 18))
-                           
-                     Spacer()
-                            HStack{
-                        InputDateTimeView(inputType: "date")
-                        InputDateTimeView(inputType: "time")
-                                
-                    }
-                    
-                            Toggle(isOn: $Acompanhates) {
-                                Text("Irá acompanhado de outro passageiro?")
-                                    .font(Font.custom("Helvetica", size: 14))
-                            }
+                        
+                        Text("Escolha seu local e data de viagem ")
+                            .font(Font.custom("Jost-Bold", size: 18))
+                            .foregroundStyle(.button)
+                        
+                        HStack{
+                            InputDateTimeView(inputType: "date")
+                            InputDateTimeView(inputType: "time")
+                        }
+                        
+                        Toggle("Irá acompanhado de outro passageiro?", isOn: $Acompanhates)
                             .scaleEffect(0.97)
-                            .toggleStyle(SwitchToggleStyle())
-                    
+                            .font(Font.custom("Helvetica", size: 14))
+                            .tint(.button)
+                        
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Local")
+                                .font(.custom("Helvetica-Bold", size: 16))
+                                .foregroundStyle(.button)
+                            
+                            Text("Selecione o heliponto.")
+                                .font(.custom("Helvetica", size: 14))
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Partida")
+                                .font(.custom("Helvetica", size: 14))
+                                .foregroundStyle(.tex)
+                            
+                            TextField("Buscar local de partida...", text: $departureInput)
+                                .font(.custom("Helvetica", size: 14))
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 15)
+                                .background(RoundedRectangle(cornerRadius: 56)
+                                    .fill(.inputButton))
+                            
+                            Spacer(minLength: 10)
+                            
+                            Text("Chegada")
+                                .font(.custom("Helvetica", size: 14))
+                                .foregroundStyle(.tex)
+                            
+                            TextField("Buscar local de partida...", text: $arrivalInput)
+                                .font(.custom("Helvetica", size: 14))
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 15)
+                                .background(RoundedRectangle(cornerRadius: 56)
+                                    .fill(.inputButton))
+                        }
+                        
+                        Map(initialPosition: cameraPosition) {
+                            
+                        }
+                        .frame(maxWidth: .infinity, minHeight: 300)
+                        .cornerRadius(10)
+                        .tint(.button)
+                        .onAppear {
+                            locationManager.requestWhenInUseAuthorization()
+                        }
+                        .mapControls {
+                            MapUserLocationButton()
+                            MapCompass()
+                            MapPitchToggle()
+                            MapScaleView()
+                        }
+                        
+                        Spacer(minLength: 40)
+                    }
                     
                 }
                 .padding(.horizontal, 54)
+                .scrollIndicators(.hidden)
             }
+            
             Spacer()
             ContentView(selectedView: "home")
                 .frame(height: 20)
