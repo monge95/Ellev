da716d43e842997166ecb3fb8e87fc84b77ae902
João Cláudio <leiloka46@gmail.com>
2026-04-13 15:42:16 -0300

    feat: Componenizando o mapa com novas funcionalidades

---
+++ ellev/componentes/CustomMap.swift
@@ -0,0 +1,18 @@
+//
+//  CustomMap.swift
+//  ellev
+//
+//  Created by JOAO CLAUDIO DOS SANTOS SOUZA on 13/04/26.
+//
+
+import SwiftUI
+
+struct CustomMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomMap()
+}
