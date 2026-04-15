ca0b7bdf69c8798809a64372d6f740c27eccc972
João Cláudio <leiloka46@gmail.com>
2026-04-15 16:36:37 -0300

    fix: Espacamento incorredo do card

--- ellev/componentes/WarningInformationCard.swift
+++ ellev/componentes/WarningInformationCard.swift
@@ -10,18 +10,17 @@ import SwiftUI
 struct WarningInformationCard: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            Text("Lembrando que o limite de peso das bagagens é de **10kg por pessoa**. ")
+            Text("Lembrando que o limite de peso das bagagens é de **10kg por pessoa**.")
                 .font(.custom("Helvetica", size: 13))
                 .foregroundStyle(.fundodeTelas)
-                .padding(.top, 15)
-                .padding(.horizontal, 27)
             
             Text("Crianças **menores de 18 anos** não podem viajar desacompanhadas e **menores de 2 anos não podem embarcar** na aeronave.")
                 .font(.custom("Helvetica", size: 13))
                 .foregroundStyle(.fundodeTelas)
-                .padding(.bottom, 15)
-                .padding(.horizontal, 27)
         }
+        .padding(.horizontal, 27)
+        .padding(.vertical, 15)
+        .frame(maxWidth: .infinity)
         .background(.card)
         .cornerRadius(5)
     }
